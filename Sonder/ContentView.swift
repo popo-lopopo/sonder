30d27a7641032c92046b3bddd999ccb0e815b260
pmielle <pm.mielle@gmail.com>
2020-05-21 15:18:07 +0200

    Initial Commit

---
+++ Sonder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sonder
+//
+//  Created by Paul Mielle on 21/05/2020.
+//  Copyright © 2020 Paul Mielle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
